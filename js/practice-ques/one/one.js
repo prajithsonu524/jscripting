9a0661709af995b06f85ea0f7fea98005eee5274
Prajith Aarya <prajith.sonu@gmail.com>
2023-01-15 22:16:36 +0530

    feat : add largesteven test

---
+++ js/practice-ques/one/one.js
@@ -0,0 +1,24 @@
+const getLargestEvenArrow = (arr) => {
+    let maxEven = -1;
+    arr.forEach((val) => {
+      if (val > maxEven && val%2==0) {
+        maxEven = val;
+      }
+    })
+    return maxEven;
+  }
+  
+  function getLargestEvenRegular(arr) {
+    let maxEven = -1;
+    arr.forEach((val) => {
+      if (val > maxEven && val%2==0) {
+        maxEven = val;
+      }
+    })
+    return maxEven;
+  }
+  
+  module.exports = {
+    getLargestEvenArrow,
+    getLargestEvenRegular
+  }
